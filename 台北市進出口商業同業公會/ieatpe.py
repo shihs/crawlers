c9d856f36c1533d78fb21c0bb1daf32691ae8c40
shihs <abc780922@gmail.com>
2017-09-26 15:45:27 +0800

    fix ieatpe.py

--- 台北市進出口商業同業公會/ieatpe.py
+++ 台北市進出口商業同業公會/ieatpe.py
@@ -7,21 +7,23 @@ import json
 
 
 data = []
-data.append(["company", "address"])
+data.append(["company"])
 
 
 url = "http://www.ieatpe.org.tw/en/data/member.ashx?qry=%7B%22type%22%3A1%2C%22input%22%3A%22%22%7D"
 res = requests.get(url)
 
-companies = res.text.encode("utf-8", "ignore")
-companies = json.loads(companies)
+# companies = res.text.encode("utf-8", "ignore")
+# companies = json.loads(companies)
 
+companies = json.loads(res.text)
 page = 0
 
+print len(companies)
 for i in companies:
 	page += 1
 	print page, i["name"]
-	data.append([i["name"], i["addr"]])
+	data.append([i["name"].encode("utf-8", "ignore")])
 
 
 
